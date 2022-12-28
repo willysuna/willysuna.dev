ff96310d4531d30fe04a1a521b0d0a74d1854e82
William <78625911+willysuna@users.noreply.github.com>
2022-12-28 13:35:28 +0100

    haha yes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<head>
+    <title>William</title>
+    <link rel="stylesheet" href="/src/style.css">
+    <script src="https://kit.fontawesome.com/057b0dec9d.js" crossorigin="anonymous"></script>
+</head>
+
+<script>
+    let date = new Date().toLocaleString("es-ES", { timeZone: "Europe/Madrid" });
+</script>
+
+<div class="perfectcenter">
+    <h1>William <img src="https://willysuna.s-ul.eu/hX459EU9" alt="emoji" width="40vw"></h1><br>
+    <h2>I make cool stuff on the internet.</h2>
+    <p><i class="fa-solid fa-clock"></i> {date}</p>
+</div>
