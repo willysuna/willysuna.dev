52eb08e3fc8f9baaaf69f972ca7b093a16c95236
William <78625911+willysuna@users.noreply.github.com>
2023-01-18 11:34:40 +0100

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
-<head>
-    <title>William</title>
-    <script src="https://kit.fontawesome.com/057b0dec9d.js" crossorigin="anonymous"></script>
-</head>
-
 <script>
-    let date = new Date().toLocaleString("es-ES", { timeZone: "Europe/Madrid" });
+  let year = new Date().getFullYear()
+  let timesinceyearistarted = year - 2017
 </script>
-
-<div class="perfectcenter">
-    <h1>William <img src="https://willysuna.s-ul.eu/hX459EU9" alt="emoji" width="40vw"></h1><br>
-    <h2>I make cool stuff on the internet.</h2>
-    <p><i class="fa-solid fa-clock"></i> {date}</p>
+<head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"><meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://kit.fontawesome.com/057b0dec9d.js" crossorigin="anonymous"></script>
+    <script src="./detect.js"></script>
+    <title>WillysunaDev - Home</title>
+</head>
+<div>
+      <nav class="fixed-top">
+        <ul>
+          <li>WillysunaDev</li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="perfectcenter flex">
+        <img src="https://avatars.githubusercontent.com/u/78625911?v=4" alt="GitHub Profile Picture">
+        <div style="padding-left: 30px;">
+            I am a teenager who is passionate about programming.
+            I have been interested in computers and technology for a long time and I have been learning how to code (from myself, and from classes) since <em data-tooltip="{timesinceyearistarted} years ago">2017</em>.
+            I am always eager to learn more and is constantly exploring new programming languages and technologies.
+            I enjoy the challenge of working through coding and life's obstacles.
+        </div>
+      </div>
 </div>
