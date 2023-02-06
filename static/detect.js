6d49168bc2a3aad70d6f9b4d861f5cc1e66b640b
William <78625911+willysuna@users.noreply.github.com>
2023-02-06 21:19:31 +0100

    Delete detect.js

--- static/detect.js
+++
@@ -1,6 +0,0 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    console.log("Known Mobile Browsers detected! Redirecting...")
-    window.location = "https://m.willysuna.dev"
-  }else{
-    console.log("No known Mobile Browsers detected!")
-  }
